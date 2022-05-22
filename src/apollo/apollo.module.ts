f8e2eade54c3d9a67defa0842feee801ed8bc29a
coder <dnstylish@gmail.com>
2022-05-23 00:48:02 +0700

    add user auth

--- src/apollo/apollo.module.ts
+++ src/apollo/apollo.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo'
 import { IntrospectAndCompose } from '@apollo/gateway'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 // import GraphQLJSON from 'graphql-type-json'
 
 @Module({
@@ -12,15 +13,18 @@ import { IntrospectAndCompose } from '@apollo/gateway'
       driver: ApolloGatewayDriver,
       useFactory: () => ({
         server: {
+          playground: false,
           debug: true,
           cors: true,
-          path: '/graphql'
+          path: '/graphql',
+          plugins: [ApolloServerPluginLandingPageLocalDefault()]
         },
         gateway: {
           debug: true,
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
-              { name: 'started', url: 'http://localhost:3069/graphql' }
+              { name: 'users', url: 'http://localhost:3069/graphql' }
+              // { name: 'auth', url: 'http://localhost:3070/graphql' }
             ]
           })
         }
