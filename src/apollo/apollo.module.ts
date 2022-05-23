87760d32fadfa3d96ae97daa56b5a8588d5a68fe
coder <dnstylish@gmail.com>
2022-05-23 09:01:04 +0700

    add user auth

--- src/apollo/apollo.module.ts
+++ src/apollo/apollo.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo'
-import { IntrospectAndCompose } from '@apollo/gateway'
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway'
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 // import GraphQLJSON from 'graphql-type-json'
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest({ request, context }) {
+    const headers = context?.req?.headers
+    for (const key in headers) {
+      const value = headers[key]
+      if (value) {
+        request.http?.headers.set(key, String(value))
+      }
+    }
+  }
+}
 
 @Module({
   imports: [
@@ -23,10 +35,13 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
           debug: true,
           supergraphSdl: new IntrospectAndCompose({
             subgraphs: [
-              { name: 'users', url: 'http://localhost:3069/graphql' }
-              // { name: 'auth', url: 'http://localhost:3070/graphql' }
+              { name: 'users', url: 'http://localhost:3069/graphql' },
+              { name: 'categories', url: 'http://localhost:3070/graphql' }
             ]
-          })
+          }),
+          buildService({ name, url }) {
+            return new AuthenticatedDataSource({ url })
+          }
         }
       })
     })
